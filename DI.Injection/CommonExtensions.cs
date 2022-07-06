fd85c3d59982c8e608a1dae43a938354a172395a
Antonio Jose <falecomoantonio@live.com>
2022-07-06 19:54:54 -0300

    Change

--- DI.Injection/CommonExtensions.cs
+++ DI.Injection/CommonExtensions.cs
@@ -14,8 +14,9 @@ namespace DI.Injection
             var ctor = type.GetConstructors()
                 .Where(c => c.IsPublic)
                 .OrderByDescending(c => c.GetParameters().Length)
-                .FirstOrDefault()
-                ?? throw new InvalidOperationException($"No suitable contructor found on type '{type}'");
+                .FirstOrDefault();
+
+            _ = ctor ?? throw new InvalidOperationException($"No suitable contructor found on type '{type}'");
 
             var injectionServices = ctor.GetParameters()
                 .Select(p => serviceProvider.GetRequiredService(p.ParameterType))
